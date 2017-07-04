fe0305ce8e7ebc5d7fc495f6218945d16f7553a4
dimi3ryi <feschenko.dmitryi@gmail.com>
2017-07-04 16:16:02 +0300

    initial commit

---
+++ backend/src/main/java/com/technoboom/ItAcademyApplication.java
@@ -0,0 +1,12 @@
+package com.technoboom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItAcademyApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/technoboom/ItAcademyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.technoboom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItAcademyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
