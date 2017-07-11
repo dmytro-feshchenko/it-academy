c7b898286b89a437e69c92b1f8666bdfd3af9d95
dimi3ryi <feschenko.dmitryi@gmail.com>
2017-07-11 21:56:53 +0300

    add dropdown component and toggle button component

---
+++ frontend/src/components/AsyncGrid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AsyncGrid extends React.Component {
+    render() {
+        return (
+            <div>Async Grid will be here</div>
+        )
+    }
+}
+
+export default AsyncGrid;
