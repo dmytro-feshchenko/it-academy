69f2ad48b5f46be60ddb78f6f729a80ef85d9d57
dimi3ryi <feschenko.dmitryi@gmail.com>
2017-07-23 22:58:17 +0300

    add logout and session constants and actions to authentication reducer

--- frontend/src/reducers/authentication.js
+++ frontend/src/reducers/authentication.js
@@ -1,6 +1,15 @@
 const LOGIN = 'authentication/LOGIN';
 const LOGIN_SUCCESS = 'authentication/LOGIN_SUCCESS';
 const LOGIN_FAIL = 'authentication/LOGIN_FAIL';
+
+const LOGOUT = 'authentication/LOGOUT';
+const LOGOUT_SUCCESS = 'authentication/LOGOUT_SUCCESS';
+const LOGOUT_FAIL = 'authentication/LOGOUT_FAIL';
+
+const GET_SESSION = 'authentication/GET_SESSION';
+const GET_SESSION_SUCCESS = 'authentication/GET_SESSION_SUCCESS';
+const GET_SESSION_FAIL = 'authentication/GET_SESSION_FAIL';
+
 const ERROR_MESSAGE = 'authentication/ERROR_MESSAGE';
 
 const initialState = {
@@ -29,6 +38,33 @@ export default function reducer(state = initialState, action) {
                 errorMessage: action.result.data.messageKey
             };
             break;
+        case LOGOUT_SUCCESS:
+            return {
+                ...state,
+                isAuthenticated: false,
+                username: null
+            };
+        case GET_SESSION:
+            return {
+                ...state,
+                loading: true
+            };
+        case GET_SESSION_SUCCESS:
+            return {
+                ...state,
+                isAuthenticated: action.result.data.authenticated || false,
+                username: action.result.data.userName,
+                errorMessage: null,
+                loading: false
+            };
+        case GET_SESSION_FAIL:
+            return {
+                ...state,
+                isAuthenticated: false,
+                username: null,
+                debugError: action.error,
+                loading: false
+            };
         case ERROR_MESSAGE:
             return {
                 ...state,
@@ -40,6 +76,7 @@ export default function reducer(state = initialState, action) {
 }
 
 // Public action creators and async actions
+
 /**
  * Displays error message during an authorization/authentication process
  * @param message
@@ -49,10 +86,39 @@ export function displayAuthError(message) {
     return { type: ERROR_MESSAGE, message };
 }
 
-export function login(token) {
-
+export function login(username, password) {
+    return {
+        types: [LOGIN, LOGIN_SUCCESS, LOGIN_FAIL],
+        promise: client => client.post('api/session', { username, password }),
+        afterSuccess: (dispatch, getState, response) => {
+            localStorage.setItem('auth-token', response.headers['x-auth-token']);
+            const routingState = getState().routing.locationBeforeTransitions.state || {};
+            // browserHistory.push(routingState.nextPathname || '');
+        }
+    }
 }
 
-export function redirectToErrorPageAfterFail() {
+export function logout() {
+    return {
+        types: [LOGOUT, LOGOUT_SUCCESS, LOGOUT_FAIL],
+        promise: (client) => client.delete('/api/session'),
+        afterSuccess: () => {
+            // browserHistory.push('login');
+        }
+    };
+}
 
+export function getSession() {
+    return {
+        types: [GET_SESSION, GET_SESSION_SUCCESS, GET_SESSION_FAIL],
+        promise: (client) => client.get('/api/session')
+    };
+}
+
+export function redirectToLoginWithMessage(messageKey) {
+    return (dispatch, getState) => {
+        const currentPath = getState().routing.locationBeforeTransitions.pathname;
+        dispatch(displayAuthError(messageKey));
+        // browserHistory.replace({pathname: '/login', state: {nextPathname: currentPath}});
+    }
 }
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -3,9 +3,13 @@ import { routerReducer as routing } from 'react-router-redux';
 
 import authentication from './authentication';
 import courses from './courses';
+import blog from './blog';
+import locale from './locale';
 
 export default combineReducers({
     authentication,
     courses,
+    blog,
+    locale,
     routing
 })
